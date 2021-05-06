645fa546a43f053ed8d42dd9e6d09b342f049806
Samvit20 <samvit999@gmail.com>
2021-05-06 18:57:31 +0530

    second

--- project.py
+++ project.py
@@ -4,15 +4,6 @@ file_name = 'text.txt'
 list_N_P=[]
 final_N_P=[]
 
-def remove_similar(*arr):
-    m=[]
-    for i in arr:
-        if (arr.count(arr[i])==1):
-            m.append(arr[i])
-        else:
-            pass
-    return(m)
-
 file_text = open(file_name).read()
 file_doc = nlp(file_text)
 # print ([token.text for token in file_doc])
@@ -28,6 +19,8 @@ for i in list_N_P:
         final_N_P.append(i)
 
 print(str(final_N_P))
+
+#VERBS
 
 print("\n")
 
