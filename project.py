e6f8ad3941a6baf9a62b10f3971b91a256b7cb7b
Grihit <grihitbudhiraja12@gmail.com>
2021-05-10 00:50:57 +0530

    Added word frequency. Verbs not working

--- project.py
+++ project.py
@@ -1,26 +1,51 @@
 import spacy
+import textacy
+from collections import Counter
 nlp = spacy.load('en_core_web_sm')
 file_name = 'text.txt'
-list_N_P=[]
-final_N_P=[]
+list_N_P = []
+final_N_P = []
 
+
+# Reading the file
 file_text = open(file_name).read()
 file_doc = nlp(file_text)
-# print ([token.text for token in file_doc])
+
 
 #NOUNS & PRONOUNS
 for word in file_doc.noun_chunks:
     list_N_P.append(str(word))
-print(list_N_P)
 
 final_N_P = []
+
 for i in list_N_P:
     if i not in final_N_P:
         final_N_P.append(i)
 
-print(str(final_N_P))
+print("Nouns are: \n"+str(final_N_P))
+# Word Frequency
+words = [token.text for token in file_doc if not token.is_stop and not token.is_punct]
+word_freq = Counter(words)
 
-#hello
+#5 commonly occurring words with their frequencies
+common_words = word_freq.most_common(5)
+print("\nCommon words are:")
+print(common_words)
+
+# Unique words
+unique_words = [word for (word, freq) in word_freq.items() if freq == 1]
+print("\nUnique words are: ")
+print(unique_words)
+
+# VERBS
+# pattern = [{'POS': 'VERB', 'OP': '?'},
+#           {'POS': 'ADV', 'OP': '*'},
+#           {'POS': 'VERB', 'OP': '+'}]
+#file_doc2 = textacy.make_spacy_doc(file_text, lang='en_core_web_sm')
+#verbs = textacy.extract.matches(file_doc2, pattern)
+# for list in verbs:
+#    print(list.text)
+
 
 print("\n")
 
